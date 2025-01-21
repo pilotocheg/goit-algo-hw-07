c3ca3b32fdd6a13355d0c4a3c506d66e3568a5af
Viacheslav Dmytriiev <v.dmytriiev@sdventures.com>
2025-01-21 19:29:09 +0100

    Fix node string formatting

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class Node:
         self.val = key
 
     def __str__(self, level=0, prefix="Root: "):
-        ret = "\\t" * level + prefix + str(self.val) + "\\n"
+        ret = "\t" * level + prefix + str(self.val) + "\n"
         if self.left:
             ret += self.left.__str__(level + 1, "L--- ")
         if self.right:
